579de988f7ae61b6640a381b35cc004b879d08d3
liushuai <770722922@qq.com>
2019-08-04 02:58:48 +0800

    feature: refine protoutils

--- core/ProtoUtils.h
+++ core/ProtoUtils.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "google/protobuf/wrappers.pb.h"
 #include "proto/cpp/Msg.pb.h"
 #include "MsgDispatcher.h"
 #include "Type.h"
@@ -14,7 +15,7 @@ namespace ProtoUtils {
 static std::atomic<SeqType> MsgSeq(0);
 
 // 默认空消息 方便接口设计
-const Msg MsgNone; // NOLINT(cert-err58-cpp)
+const StringValue DataNone; // NOLINT(cert-err58-cpp)
 
 /**
  * 创建Cmd消息
@@ -22,12 +23,12 @@ const Msg MsgNone; // NOLINT(cert-err58-cpp)
  * @param data
  * @return
  */
-inline Msg CreateCmdMsg(CmdType cmd, const Message& data = MsgNone) {
+inline Msg CreateCmdMsg(CmdType cmd, const Message& data = DataNone) {
     Msg msg;
     msg.set_type(Msg::COMMAND);
     msg.set_cmd(cmd);
     msg.set_seq(MsgSeq++);
-    if (&data != &MsgNone) {
+    if (&data != &DataNone) {
         msg.mutable_data()->PackFrom(data);
     }
     return msg;
@@ -40,12 +41,17 @@ inline Msg CreateCmdMsg(CmdType cmd, const Message& data = MsgNone) {
  * @param success 成功/失败
  * @return
  */
-inline Msg CreateRspMsg(SeqType seq, const Message& data = MsgNone, bool success = true) {
+template <typename T = Message>
+inline Msg CreateRspMsg(SeqType seq, const T& data = DataNone, bool success = true) {
+    //  data为Message类型 而不是Msg类型 防止误用
+    static_assert(std::is_base_of<Message, T>(), "data should be a Message!");
+    static_assert(!std::is_base_of<Msg, T>(), "data should not be a Msg!");
+
     Msg msg;
     msg.set_type(Msg::RESPONSE);
     msg.set_success(success);
     msg.set_seq(seq);
-    if (&data != &MsgNone) {
+    if ((intptr_t*)&data != (intptr_t*)&DataNone) {
         msg.mutable_data()->PackFrom(data);
     }
     return msg;
@@ -71,7 +77,7 @@ inline std::string CreatePayload(const Msg& msg) {
  * @return
  */
 template <CmdType cmd>
-inline std::string CreateCmdPayload(const Message &data = MsgNone, const RspCallback& cb = nullptr) {
+inline std::string CreateCmdPayload(const Message &data = DataNone, const RspCallback& cb = nullptr) {
     std::string payload;
     auto msg = CreateCmdMsg(cmd, data);
     auto ret = msg.SerializeToString(&payload);
